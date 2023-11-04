a596934f5fd5a7b2a19fe128847bb1c483b37afa
Yaroslav Bulatov <yaroslavvb@gmail.com>
2023-11-03 20:08:44 -0700

    Training to 63% with standard learning rate
    dataset size:  1000
    accuracy: 0.11, test_loss: 0.50, old train loss: 0.51, new train loss: 0.51
    accuracy: 0.57, test_loss: 0.38, old train loss: 0.35, new train loss: 0.35
    accuracy: 0.60, test_loss: 0.35, old train loss: 0.26, new train loss: 0.26
    accuracy: 0.61, test_loss: 0.34, old train loss: 0.20, new train loss: 0.20
    accuracy: 0.62, test_loss: 0.34, old train loss: 0.15, new train loss: 0.15
    accuracy: 0.63, test_loss: 0.35, old train loss: 0.13, new train loss: 0.13
    accuracy: 0.63, test_loss: 0.35, old train loss: 0.11, new train loss: 0.11
    accuracy: 0.63, test_loss: 0.36, old train loss: 0.10, new train loss: 0.10
    accuracy: 0.63, test_loss: 0.36, old train loss: 0.09, new train loss: 0.09

--- train/train_linear_mnist.py
+++ train/train_linear_mnist.py
@@ -12,7 +12,7 @@ class Net(nn.Module):
     def __init__(self, d0):
         super(Net, self).__init__()
         self.d0 = d0
-        self.fc1 = nn.Linear(d0, 10)
+        self.fc1 = nn.Linear(d0, 10, bias=False)
 
     def forward(self, x):
         x = x.reshape((-1, self.d0))
@@ -24,32 +24,28 @@ root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 def main():
+    do_kaczmarz = False
+    do_kaczmarz = True
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     torch.manual_seed(1)
 
     dataset_size = 1000
-    train_kwargs = {'batch_size': 10, 'num_workers': 0, 'shuffle': False}
-    test_kwargs = {'batch_size': 10}
+    train_kwargs = {'batch_size': 1, 'num_workers': 0, 'shuffle': False}
+    test_kwargs = {'batch_size': 1}
 
     # do_squared_loss = False
     do_squared_loss = True
     loss_type = 'LeastSquares' if do_squared_loss else 'CrossEntropy'
     loss_fn = kac.least_squares_loss if do_squared_loss else kac.combined_nll_loss
 
-    train_dataset = kac.NumpyDataset(root + '/data/mnistTrainW.npy',
-                                     root + '/data/mnistTrain-labels.npy',
-                                     dataset_size=dataset_size)
+    train_dataset = kac.TinyMNIST(train=True, loss_type=loss_type, whiten_and_center=True, dataset_size=dataset_size)
     train_loader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
 
-    test_dataset = kac.NumpyDataset(root + '/data/mnistTestW.npy',
-                                    root + '/data/mnistTest-labels.npy',
-                                    dataset_size=dataset_size)
-
-    # train_dataset = kac.TinyMNIST(train=True, whiten=True)
-    # test_dataset = kac.TinyMNIST(train=False, whiten=True, whiteningMatrix=train_dataset.whiteningMatix)
+    test_dataset = kac.TinyMNIST(train=False, loss_type=loss_type, whiten_and_center=True, dataset_size=dataset_size)
 
     model = Net(d0=28 * 28).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=1/10, momentum=0.)
+    optimizer = optim.SGD(model.parameters(), lr=2/10, momentum=0.)
 
     print("dataset size: ", len(train_dataset))
     for epoch in range(1, 10):
@@ -78,14 +74,19 @@ def main():
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
-            output = model(data)
-            # loss = F.nll_loss(output, target)
+            if do_kaczmarz:
+                with kac.module_hook(kac.kaczmarz_grad_linear):
+                    output = model(data)
+            else:
+                output = model(data)
             loss = loss_fn(output, target)
             loss.backward()
+
+            if do_kaczmarz:
+                kac.copy_custom_grad_to_grad(model)
+
             optimizer.step()
 
-            output = model(data)
-            # new_loss = F.nll_loss(output, target)
             new_loss = loss_fn(output, target)
         print(
             f"accuracy: {correct / total:0.2f}, test_loss: {test_loss:.2f}, old train loss: {loss.item():.2f}, new train loss: {new_loss.item():.2f}")
