fix covariance computation

kaczmarz/kac.py
@@ -337,13 +337,15 @@ class CustomMNIST(datasets.MNIST):
                 data = self.data
                 data = data.reshape(data.shape[0], -1)
                 A = data - torch.mean(data.float(), dim=1, keepdim=True)
-                cov = A.T @ A
+                #  cov = A.T @ A
+                cov = getCov(A)
                 W = isymsqrtStable(cov)
 
                 B = A @ W
 
                 # 712 non-zero eigs, 60000 examples, normalize to have examples with unit norm on average
-                W = W * np.sqrt(60000 / 712)
+                # W = W * np.sqrt(60000 / 712)
+                W = W / np.sqrt(712)
                 CustomMNIST.mnistTrainWhiteningMatrix = torch.tensor(W, device=device)
 
         if dataset_size > 0:
@@ -594,7 +596,8 @@ def effRank(mat):
 
 
 def getCov(mat):
-    return mat.T @ mat
+    m = mat.shape[0]
+    return mat.T @ mat / m
 
 
 @contextmanager

kaczmarz/optimize_test.py
@@ -716,7 +716,7 @@ def test_whitening():
     np.testing.assert_allclose(kac.effRank(cov), 712)
 
 
-def test_mnist_whitening():
+def test_whitened_mnist():
     dataset = kac.CustomMNIST(train=True, whiten_and_center=True)
     loader = torch.utils.data.DataLoader(dataset, batch_size=60000, shuffle=False)
     X, Y = next(iter(loader))
@@ -725,12 +725,13 @@ def test_mnist_whitening():
 
     assert 60000 <= np.trace(X.T @ X) <= 60040
 
+    print("Average norm2: ", np.trace(kac.getCov(X)))
+
     dataset = kac.CustomMNIST(train=False, whiten_and_center=True)
     loader = torch.utils.data.DataLoader(dataset, batch_size=10000, shuffle=False)
     X, Y = next(iter(loader))
     X = X.double().reshape(-1, 28 * 28)
-    assert(int(kac.effRank(kac.getCov(X))) == 33)
-
+    assert(int(kac.effRank(kac.getCov(X))) == 58)
 
 
 if __name__ == '__main__':